cf5f9758ee20cce9abd4d1b301265db4524a446a
belizario87 <leonardobelizario88@gmail.com>
2023-05-09 20:47:12 -0300

    card creation, adjust navbar and btn

--- employeemanangerapp/src/app/employee.service.ts
+++ employeemanangerapp/src/app/employee.service.ts
@@ -15,17 +15,17 @@ export class EmployeeService {
     constructor(private http: HttpClient) { }
 
     public getEmployees(): Observable<Employee[]> {
-        return this.http.get<Employee[]>(`${this.apiServerUrl}/employee/all`);
+        return this.http.get<Employee[]>(`${this.apiServerUrl}/employees/all`);
     }
 
     public addEmployees(employee: Employee): Observable<Employee> {
-        return this.http.post<Employee>(`${this.apiServerUrl}/employee/add`, employee);
+        return this.http.post<Employee>(`${this.apiServerUrl}/employees/add`, employee);
     }
     public updateEmployees(employee: Employee): Observable<Employee> {
-        return this.http.put<Employee>(`${this.apiServerUrl}/employee/update`, employee);
+        return this.http.put<Employee>(`${this.apiServerUrl}/employees/update`, employee);
     }
     public deleteEmployees(employeeId: number): Observable<void> {
-        return this.http.delete<void>(`${this.apiServerUrl}/employee/delete/${employeeId}`);
+        return this.http.delete<void>(`${this.apiServerUrl}/employees/delete/${employeeId}`);
     }
 
 
